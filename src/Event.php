5e0fdfa08f927aaa85275ff30f7ca1267adcd11e
Caen De Silva <caen@desilva.se>
2022-04-23 19:16:43 +0200

    Add the Event model

---
+++ src/Event.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OpenEvents;
+
+use JetBrains\PhpStorm\ArrayShape;
+
+class Event
+{
+    public string $event;
+    public ?string $data = null;
+
+    public function __construct(string $event, ?string $data = null)
+    {
+        $this->event = $event;
+        $this->data = $data;
+    }
+
+    #[ArrayShape(['event' => "string", 'data' => "null|string"])]
+    public function toArray(): array
+    {
+        return [
+            'event' => $this->event,
+            'data' => $this->data,
+        ];
+    }
+
+    public function dispatchEvent(): void
+    {
+        (new Client)->dispatchRequest($this);
+    }
+
+    public static function dispatch(string $event, ?string $data = null): void
+    {
+        (new self($event, $data))->dispatchEvent();
+    }
+}
