9e5f036ab5483a5b6aff257b473050f75fca44ff
Caen De Silva <caen@desilva.se>
2022-04-23 17:45:19 +0200

    Rename host to endpoint to match documentation

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
  * Configure your OpenEvents settings here.
  */
 return [
-	// Your API token 
+	// Your API token
 	'token' => env('OPENEVENTS_TOKEN'),
 
     // Your API endpoint
-    'host' => env('OPENEVENTS_HOST'),
+    'endpoint' => env('OPENEVENTS_ENDPOINT'),
 ];
