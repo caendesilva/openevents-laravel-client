6998ff026c5ddd94990b1f6114a1db57ba6c1335
Caen De Silva <caen@desilva.se>
2022-04-24 19:18:05 +0200

    Add an example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OpenEvents\LaravelClient\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
